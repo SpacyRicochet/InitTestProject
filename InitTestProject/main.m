fda5fd0a29d0325d99e761c65043c82c34c0d866
Bruno Scheele <bruno@noodlewerk.com>
2016-09-06 20:01:59 +0200

    Initial Commit

---
+++ InitTestProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  InitTestProject
+//
+//  Created by Bruno Scheele on 06/09/16.
+//  Copyright © 2016 Noodlewerk Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
